209c2fbbfb4f755ade0c1b52223f67165f4b7e33
abibinu <abikochu007@gmail.com>
2026-06-07 07:41:12 +0530

    modularized seeding

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,29 +1,11 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
-using GameStore.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
-var connString = "Data Source=Gamestore.db";
-builder.Services.AddSqlite<GameStoreContext>(
-    connString,
-    optionsAction: options => options.UseSeeding((context, _) =>
-    {
-        if (!context.Set<Genre>().Any())
-        {
-            context.Set<Genre>().AddRange(
-                new Genre { Name = "Fighting" },
-                new Genre { Name = "RPG" },
-                new Genre { Name = "PLatformer" },
-                new Genre { Name = "Racing" },
-                new Genre { Name = "Sports" }
-            );
-            context.SaveChanges();
-        }
-    })
-);
+builder.AddGameStoreDb();
 
 var app = builder.Build();
 
