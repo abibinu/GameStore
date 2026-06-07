825ff33b249b8261f1ac5117cec2c1bc032bca56
abibinu <abikochu007@gmail.com>
2026-06-07 07:22:15 +0530

    implemented sqlite

---
+++ GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public static class DataExtensions
+{
+    public static void MigrateDb(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var DbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        DbContext.Database.Migrate();
+    }
+}
